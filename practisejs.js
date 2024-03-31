234ff791bdbb62fccd70fef91fdcaf5ca8066144
Nawin Chander <nawinnapzy289@gmail.com>
2024-03-31 22:10:12 +0530

    files need to added

---
+++ practisejs.js
@@ -0,0 +1,11 @@
+let score = 0
+score = 10
+console.log("The score is " + score);
+
+let amount = 1000
+console.log("Your bill amount is "+ amount);
+
+const pi = 3.14
+
+let name = prompt('what is your name?')
+console.log('Hi', name)
